a2b73d9ee8aa6c35bad381fdfb6d6189642b51f2
Victor  Kabike <56153857+victorkabike-ios@users.noreply.github.com>
2024-01-11 23:22:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-// Smooth scrolling for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-// Back to top functionality
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
 }
